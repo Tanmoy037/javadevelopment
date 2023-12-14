78dcd9c618684df6f116ab38ae21f7606b375475
Tanmoy037 <tanmoysantra67@gmail.com>
2023-12-14 19:33:40 +0530

    assingment

--- day3.java
+++ day3.java
@@ -1,3 +1,17 @@
 public class day3 {
+   public static void main(String[] args) {
+    // Two_Darray using user input and also will take lenght form user, and create a new array using old Two_Darray sum.(Using while loop)
     
+
+
+    // pyramid star printing
+    for (int i = 0; i < 5; i++){
+        for (int j = i; j < 5; j++){
+            System.out.print("*" + " ");
+        }
+        System.out.println(" ");
+
+    }
+
+   } 
 }
