b8fdb28315fd6d599a0135ec58a0b14f693f82cc
Tanmoy037 <tanmoysantra67@gmail.com>
2023-12-15 09:11:17 +0530

    day4 assignment

--- day4.java
+++ day4.java
@@ -2,6 +2,8 @@ public class day4 {
     public static void main(String[] args) {
         Innerday4 star_printer= new Innerday4();
         star_printer.star(6);
+        Innerday4_1 calc = new Innerday4_1();
+        calc.addition(5,6);
     }
 
 
@@ -19,6 +21,17 @@ class Innerday4 {
 
         }
         
+    }
+
+    
+}
+
+
+class Innerday4_1 {
+    public void addition(int a, int b){
+        int sum = (a + b);
+        System.out.println("addition " + sum);
+
     }
 
     
