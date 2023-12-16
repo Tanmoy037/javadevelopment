f4b14b5f20aa39516e7403f789cf5e872c533e9b
Tanmoy037 <tanmoysantra67@gmail.com>
2023-12-16 16:21:06 +0530

    day6 added

--- day6.java
+++ day6.java
@@ -1,3 +1,4 @@
+// Check two array using a method where it will compare and return boolen value.
 public class day6 {
     public static void main(String[] args) {
         Arrayckecker chek_arr = new Arrayckecker();
