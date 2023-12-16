6221d7e066040ee6004469edfbd5cdbb687e18fa
Tanmoy037 <tanmoysantra67@gmail.com>
2023-12-16 16:20:51 +0530

    day6 added

---
+++ day6.java
@@ -0,0 +1,29 @@
+public class day6 {
+    public static void main(String[] args) {
+        Arrayckecker chek_arr = new Arrayckecker();
+        int[] arr1 = {1, 2, 3};
+        int[] arr2 = {4, 5, 6};
+        boolean chekArrOutput = chek_arr.arrayChecker(arr1,arr2);
+        System.out.println(chekArrOutput);
+        
+    }
+}
+
+class  Arrayckecker {
+    public boolean arrayChecker(int []arr1 , int []arr2){
+        for (int i = 0; i < arr1.length; i++){
+            for (int j = 0; j < arr2.length; j++){
+                if (arr1[i] == arr2[j]){
+                    return true;
+                }
+            }
+        }
+ 
+        return false;
+
+
+        
+    }
+
+    
+}
