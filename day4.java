d813e60db6eb5db830c33273729364fba4c24420
Tanmoy037 <tanmoysantra67@gmail.com>
2023-12-17 21:17:00 +0530

    day7.txt added

--- day4.java
+++ day4.java
@@ -2,9 +2,12 @@ public class day4 {
     public static void main(String[] args) {
         // Innerday4 star_printer= new Innerday4();
         // star_printer.star(6);
-        Innerday4_1 calc = new Innerday4_1();
-        double new_var = calc.addition(5.0,6.0);
-        System.out.println(new_var);
+        // Innerday4_1 calc = new Innerday4_1();
+        // double new_var = calc.addition(5.0,6.0);
+        // System.out.println(new_var);
+        Andvance_calculator printer = new Andvance_calculator();
+        printer.remainder(5,6,7);
+
     }
 
 
@@ -26,7 +29,12 @@ public class day4 {
 
     
 // }
-
+class  Andvance_calculator extends Innerday4_1{
+    public int remainder(int a, int b, int c){
+        System.out.println(" advance class called ");
+        return a + b + c;
+    }
+}
 
 class Innerday4_1 {
     public int addition(int a, int b){
@@ -51,6 +59,10 @@ class Innerday4_1 {
     }
     public double addition(double a, double b){
         return (a+b);
+    }
+    public int remainder(int a, int b){
+        System.out.println(" normal method callled");
+        return a+b;
     }
 
     
