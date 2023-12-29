3b5fdec567966838a8b0f9b1e0746dc062c316b3
Tanmoy037 <tanmoysantra67@gmail.com>
2023-12-30 00:28:54 +0530

    taking input from user for array

--- day10.java
+++ day10.java
@@ -1,3 +1,14 @@
+import java.util.*;
+
 public class day10 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String[] str = new String[4];
+        for (int i = 0; i < str.length; i++){
+            str[i] = in.next();
+        }
+
+        System.out.println(Arrays.toString(str));
+    }
     
 }
