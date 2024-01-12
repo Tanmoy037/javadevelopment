dc1d0c77b1e2825883a6837c961129f266824634
Tanmoy037 <tanmoysantra67@gmail.com>
2024-01-12 23:56:43 +0530

    get_users api added

--- RestApi-Controller/src/main/java/com/example/RestApiController/MyControllers.java
+++ RestApi-Controller/src/main/java/com/example/RestApiController/MyControllers.java
@@ -1,9 +1,43 @@
 package com.example.RestApiController;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @RestController // Annotation --> In this class I will write the APIs
-class MyControllers {
+public class MyControllers {
+
+    //DB Purpose
+    HashMap<Integer,User> users = new HashMap<>();
+
+    @GetMapping("/get_users")
+    public List<User> getAllUser(){
+        //Server side logic
+        List<User> listOfUsers = new ArrayList<>();
+
+        for(User user : users.values()){
+            listOfUsers.add(user);
+        }
+
+//        for(Map.Entry<Integer,User> entry:users.entrySet()){
+//
+//           listOfUsers.add(entry.getValue());
+//
+//
+//        }
+        return listOfUsers;
+
+    }
+    @GetMapping("/get_user")
+    public User getUser(@RequestParam("id")int id){
+
+    }
+
 
 
 }
