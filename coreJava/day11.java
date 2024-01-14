ccd73497de74179a8feee9bcbd84a987f859c0f5
Tanmoy037 <tanmoysantra67@gmail.com>
2024-01-14 21:18:12 +0530

    oops started

--- coreJava/day11.java
+++ coreJava/day11.java
@@ -1,6 +1,27 @@
+//  oops in java
 public class day11 {
     public static void main(String[] args) {
-        
+        Pen pen1 = new Pen();
+        pen1.color = "blue";
+        pen1.type = "gel";
+
+        pen1.write();
     }
     
 }
+
+
+class Pen {
+    String color;
+    String type;
+
+    public void write(){
+        System.out.println("writing something");
+    }
+    public void printColor(){
+        System.out.println(this.color);
+
+    }
+}
+
+
