6a7721b4e6f30d16777d7690ba1abaae403c2457
Tanmoy037 <tanmoysantra67@gmail.com>
2024-01-15 23:42:28 +0530

    inheritance added

--- coreJava/day11.java
+++ coreJava/day11.java
@@ -17,19 +17,34 @@ public class day11 {
         s1.name = "Tanmoy";
         s1.age = 24;
 
-        s1.printInfo();
+        s1.printInfo(s1.name, s1.age);
     }
     
 }
 
+// polymorphism
 class Student{
     String name;
     int age;
 
-    public void printInfo(){
-        System.out.println(this.name);
-        System.out.println(this.age);
+    public void printInfo(String name){
+        System.out.println(name);
+        
     }
+    public void printInfo(int age) {
+        System.out.println(age);
+    }
+    public void printInfo(String name, int age){
+        System.out.println(name + " " + age);
+    }
+}
+// Inheritence
+class Shape {
+    String color;
+}
+
+class Triangle extends Shape {
+    
 }
 
 
