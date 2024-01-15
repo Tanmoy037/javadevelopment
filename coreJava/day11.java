a592a49cf684f8fb2135b562cc235228d59eb4e0
Tanmoy037 <tanmoysantra67@gmail.com>
2024-01-15 21:38:18 +0530

    classes and constracturs added

--- coreJava/day11.java
+++ coreJava/day11.java
@@ -1,13 +1,35 @@
 //  oops in java
 public class day11 {
     public static void main(String[] args) {
-        Pen pen1 = new Pen();
-        pen1.color = "blue";
-        pen1.type = "gel";
+    //     Pen pen1 = new Pen();
+    //     pen1.color = "blue";
+    //     pen1.type = "gel";
 
-        pen1.write();
+    //     pen1.write();
+
+    //     Pen pen2 = new Pen();
+    //     pen2.color = "black";
+    //     pen2.type = "ballpoint";
+
+    //     pen1.printColor();
+    //     pen2.printColor();
+        Student s1 = new Student();
+        s1.name = "Tanmoy";
+        s1.age = 24;
+
+        s1.printInfo();
     }
     
+}
+
+class Student{
+    String name;
+    int age;
+
+    public void printInfo(){
+        System.out.println(this.name);
+        System.out.println(this.age);
+    }
 }
 
 
