d7310e5f0b708bf1e56b0d4187c48055dbc5955f
Tanmoy037 <tanmoysantra67@gmail.com>
2024-01-19 10:30:47 +0530

    new projected added

---
+++ RestApi-Proj2/src/main/java/com/example/RestApiProj2/MyControllers.java
@@ -0,0 +1,23 @@
+package com.example.RestApiProj2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+@RestController
+class MyControllers {
+
+    HashMap<Integer,User> users = new HashMap<>();
+
+
+    @GetMapping()
+    List<User> getAllUsers(){
+        List<User>listOfUsers = new ArrayList<>();
+
+        for(User user:users.values()){
+    }
+
+}
---
+++ RestApi-Proj2/src/main/java/com/example/RestApiProj2/User.java
@@ -0,0 +1,53 @@
+package com.example.RestApiProj2;
+
+public class User  {
+
+    private int id;
+
+    private String name;
+
+    private String country;
+
+    private int age;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public User(int id, String name, String country, int age) {
+        this.id = id;
+        this.name = name;
+        this.country = country;
+        this.age = age;
+
+
+    }
+}
