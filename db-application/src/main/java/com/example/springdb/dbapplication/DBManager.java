ea77a907a2d128e5b67c8e584b9e361bb3ad31dd
Tanmoy037 <tanmoysantra67@gmail.com>
2024-02-03 00:01:57 +0530

    db connection added

--- db-application/src/main/java/com/example/springdb/dbapplication/DBManager.java
+++ db-application/src/main/java/com/example/springdb/dbapplication/DBManager.java
@@ -2,15 +2,17 @@ package com.example.springdb.dbapplication;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBManager {
 
     public static Connection connection;
 
-    public static Connection getConnection(){
+    public static Connection getConnection() throws SQLException {
 
         if(connection==null){
-            connection = DriverManager.getConnection("jdbc:mysql//localhost:3306");
+            connection = DriverManager.getConnection("jdbc:mysql//localhost:3306","root","Tanmoy@2000");
         }
+        return connection;
     }
 }
