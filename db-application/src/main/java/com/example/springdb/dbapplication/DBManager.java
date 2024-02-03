fabd3366159850fabe9105e7e20238540df5ff11
Tanmoy037 <tanmoysantra67@gmail.com>
2024-02-03 17:52:31 +0530

    completed and some debug remains

--- db-application/src/main/java/com/example/springdb/dbapplication/DBManager.java
+++ db-application/src/main/java/com/example/springdb/dbapplication/DBManager.java
@@ -1,18 +1,61 @@
 package com.example.springdb.dbapplication;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import org.springframework.stereotype.Component;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
 
 public class DBManager {
+
+
+    DBManager() throws SQLException{
+        connection = getConnection();
+        createTable();
+    }
 
     public static Connection connection;
 
     public static Connection getConnection() throws SQLException {
 
         if(connection==null){
-            connection = DriverManager.getConnection("jdbc:mysql//localhost:3306","root","Tanmoy@2000");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306","root","Tanmoy@2000");
         }
         return connection;
+    }
+    public void createTable() throws SQLException {
+        String sql = "create table if not exists student_info(id INT primary key auto_increment,age INT,name VARCHAR(30))";
+
+        Statement st = connection.createStatement();
+
+        st.execute(sql);
+    }
+    public void insertInfo(Student student) throws SQLException{
+        String sql = "insert into student_info(name,age) values("+student.getName()+","+student.getAge()+")";
+
+        Statement st = connection.createStatement();
+        int rows = st.executeUpdate(sql);
+        System.out.println("The no of rows effected"+rows);
+    }
+
+    public List<String> getInfo() throws SQLException {
+
+        String sql = "select * form student_info";
+
+        Statement st = connection.createStatement();
+
+        ResultSet resultSet = st.executeQuery(sql);
+
+        System.out.println(resultSet);
+
+        List<String> studentList = new ArrayList<>();
+
+        while (resultSet.next()){
+            String string = resultSet.getString(1);
+            studentList.add(string);
+        }
+        return studentList;
     }
 }
--- db-application/src/main/java/com/example/springdb/dbapplication/StudentController.java
+++ db-application/src/main/java/com/example/springdb/dbapplication/StudentController.java
@@ -1,22 +1,33 @@
 package com.example.springdb.dbapplication;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.SQLException;
 import java.util.List;
 
 @RestController
 public class StudentController {
 
+    @Autowired
+    DBManager dbManagerObj;
+
     @GetMapping("/getStudent")
-    List<Student> getStudents(){
+    List<String> getStudents() throws SQLException {
+
+        return dbManagerObj.getInfo();
 
     }
 
     @PostMapping("/createStudent")
-    public void createStudent(@RequestBody() Student student){
+    public void createStudent(@RequestBody() Student student) throws SQLException {
+
+        //add into the DB
+
+        dbManagerObj.insertInfo(student);
 
     }
 
