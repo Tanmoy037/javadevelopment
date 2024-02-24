500afa15850f74c82f0818f644f083b3cf4e1492
Tanmoy037 <tanmoysantra67@gmail.com>
2024-02-24 14:01:53 +0530

    jpa project updated fixed error,added a default constructor in the class

--- spring.jpa-repository/src/main/java/com/example/db/spring/jparepository/Controllers/MovieController.java
+++ spring.jpa-repository/src/main/java/com/example/db/spring/jparepository/Controllers/MovieController.java
@@ -2,6 +2,8 @@ package com.example.db.spring.jparepository.Controllers;
 
 import com.example.db.spring.jparepository.Models.Movie;
 import com.example.db.spring.jparepository.Service.MovieServices;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,36 +12,52 @@ import java.util.List;
 @RestController
 public class MovieController {
 
+    private static final Logger log = LoggerFactory.getLogger(MovieController.class);
 
     @Autowired
     MovieServices movieServices;
 
     @GetMapping("/getMovies")
-    private List<Movie> getAllMovies(){
-
-        return movieServices.getAllMovies();
-
+    private List<Movie> getAllMovies() {
+        try {
+            log.info("Endpoint called: GET /getMovies");
+            return movieServices.getAllMovies();
+        } catch (Exception e) {
+            log.error("Error occurred while fetching movies", e);
+            throw new RuntimeException("Failed to fetch movies");
+        }
     }
-
-
 
     @PostMapping("/createMovie")
-    public void createMovie(@RequestBody Movie movie){
-        movieServices.createMovie(movie);
+    public void createMovie(@RequestBody Movie movie) {
+        try {
+            log.info("Endpoint called: POST /createMovie");
+            movieServices.createMovie(movie);
+        } catch (Exception e) {
+            log.error("Error occurred while creating movie", e);
+            throw new RuntimeException("Failed to create movie");
+        }
     }
-    @PutMapping("/updateMovie")
-    public void updateMovie(@RequestBody Movie movie){
 
-        movieServices.updateMovie(movie);
+    @PutMapping("/updateMovie")
+    public void updateMovie(@RequestBody Movie movie) {
+        try {
+            log.info("Endpoint called: PUT /updateMovie");
+            movieServices.updateMovie(movie);
+        } catch (Exception e) {
+            log.error("Error occurred while updating movie", e);
+            throw new RuntimeException("Failed to update movie");
+        }
     }
 
     @DeleteMapping("/deleteMovie")
-    public void deleteMovieByName(@RequestBody Movie movie){
-        movieServices.deleteMovieByName(movie);
-
+    public void deleteMovieByName(@RequestBody Movie movie) {
+        try {
+            log.info("Endpoint called: DELETE /deleteMovie");
+            movieServices.deleteMovieByName(movie);
+        } catch (Exception e) {
+            log.error("Error occurred while deleting movie", e);
+            throw new RuntimeException("Failed to delete movie");
+        }
     }
-
-
-
-
 }
--- spring.jpa-repository/src/main/java/com/example/db/spring/jparepository/Models/Movie.java
+++ spring.jpa-repository/src/main/java/com/example/db/spring/jparepository/Models/Movie.java
@@ -16,35 +16,40 @@ public class Movie {
     private int time;
     private double rating;
 
-    public void setName(String name) {
+    // Default constructor
+    public Movie() {
+        // Default values or initialization can be done here if needed
+    }
+
+    // Parameterized constructor
+    public Movie(String name, int time, double rating) {
         this.name = name;
-
-    }
-
-    public void setTime(int time) {
         this.time = time;
-
-    }
-
-    public void setRating(double rating) {
         this.rating = rating;
     }
 
+    // Getters and setters
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public int getTime() {
         return time;
     }
 
+    public void setTime(int time) {
+        this.time = time;
+    }
+
     public double getRating() {
         return rating;
     }
 
-    public Movie(String name, int time, double rating) {
-        this.name = name;
-        this.time = time;
+    public void setRating(double rating) {
         this.rating = rating;
     }
 }
